aa86fc78e12b1f955a4d6b16c618128892d7cddb
DIVYAM\Divyam <divyamaggarwal88@gmail.com>
2021-08-21 17:58:37 +0530

    commit

---
+++ Scripts/GameHandler.cs
@@ -0,0 +1,34 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour {
+
+    [SerializeField] private Snake snake;
+
+    private LevelGrid levelGrid;
+
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+
+        levelGrid = new LevelGrid(20, 20);
+
+        snake.Setup(levelGrid);
+        levelGrid.Setup(snake);
+    }
+
+}
