e983c211da1a4cebe79908bdab82c8b57a8bea95
DIVYAM\Divyam <divyamaggarwal88@gmail.com>
2021-08-25 15:10:36 +0530

    commit 2

--- Scripts/GameHandler.cs
+++ Scripts/GameHandler.cs
@@ -1,20 +1,8 @@
-﻿/* 
-    ------------------- Code Monkey -------------------
-
-    Thank you for downloading this package
-    I hope you find it useful in your projects
-    If you have any questions let me know
-    Cheers!
-
-               unitycodemonkey.com
-    --------------------------------------------------
- */
+﻿
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using CodeMonkey;
-using CodeMonkey.Utils;
 
 public class GameHandler : MonoBehaviour {
 
